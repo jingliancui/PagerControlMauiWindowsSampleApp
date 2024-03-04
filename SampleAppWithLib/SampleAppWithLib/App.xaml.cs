d09a2144c7bb89580b867eab0e3b04394b20582a
Eric <ericcui@qq.com>
2024-03-04 17:34:47 +0800

    Add sample app's code

---
+++ SampleAppWithLib/SampleAppWithLib/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleAppWithLib
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SampleAppWithLib/SampleAppWithLib/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleAppWithLib
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
